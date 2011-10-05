3c68bfcf0582eaf7dd1480d6e367a0f08a99cce6
Brian Gardner <bgardner7@gatech.edu>
2011-10-04 21:42:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockTracker::Application.config.secret_token = 'cd811c23dc7a0a11a0f89b989408f6af245618f3ca3b8d4b0bbd5f6c9894230ccc54d4a570af5787025c9728d2ed0df9c48be00caf53018ea0d671c94045209f'
