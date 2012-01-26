5f271e7193cba7f947cdffdd91f94d972016e1f8
Brian Gardner <brngardner@gmail.com>
2012-01-26 18:59:08 -0500

    Changed the main navigation button to redirect to the user's page if they are logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def main_button
+    if current_user
+      link_to "Stock Tracker", user_path(current_user.id)
+    else
+      link_to "Stock Tracker", "/"
+    end
+  end
 end
